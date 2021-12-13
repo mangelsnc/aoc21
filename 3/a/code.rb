959e8e22b41c1b9a53fe1e8561dc93d51157d9d8
Miguel Ángel Sánchez Chordi <mangel.snc@gmail.com>
2021-12-13 09:55:35 +0100

    Day three. Part One.

---
+++ 3/a/code.rb
@@ -0,0 +1,36 @@
+input_data = File.read("input.txt").split("\n")
+
+most_common = []
+least_common = []
+
+zeroes = Array.new(12, 0)
+ones = Array.new(12, 0)
+
+input_data.each { |binary|
+  digits = binary.split('')
+
+  digits.each_with_index { |digit, index|
+    if digit.to_i == 0
+      zeroes[index] = zeroes[index] + 1
+    else
+      ones[index] = ones[index] + 1
+    end
+  }
+}
+
+for index in (0..11) do
+  if zeroes[index] > ones[index]
+    most_common[index] = 0
+    least_common[index] = 1
+  else
+    most_common[index] = 1
+    least_common[index] = 0
+  end
+end
+
+gamma_rate = most_common.join.to_i(2)
+epsilon_rate = least_common.join.to_i(2)
+
+power_consumption = gamma_rate * epsilon_rate
+
+pp power_consumption
